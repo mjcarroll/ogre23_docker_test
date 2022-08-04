00228ea24a135e441ed2524d977de1498f15fec3
Michael Carroll <michael@openrobotics.org>
2022-08-04 08:13:54 -0500

    Add test for major version only
    Signed-off-by: Michael Carroll <michael@openrobotics.org>

--- run.sh
+++ run.sh
@@ -2,6 +2,4 @@
 
 cd /workspace
 colcon build --event-handlers=console_cohesion+ --packages-select gz-cmake3
-colcon build --event-handlers=console_cohesion+ --packages-select ogre-2.1 
-colcon build --event-handlers=console_cohesion+ --packages-select ogre-2.2 
-colcon build --event-handlers=console_cohesion+ --packages-select ogre-2.3 
+colcon build --event-handlers=console_cohesion+ --packages-above gz-cmake3
